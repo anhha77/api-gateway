79cb8ac638c2fe8c10f503216bd93d8be9347704
anhha77 <kentucky772001@gmail.com>
2025-03-09 10:43:18 +0700

    update

--- src/domain/payment/payment.service.ts
+++ src/domain/payment/payment.service.ts
@@ -1,17 +1,24 @@
-import {Inject, Injectable} from "@nestjs/common";
-import { ClientKafka } from "@nestjs/microservices";
-import { PaymentDto } from "src/app/dto/payment.dto";
-import { PaymentSerialize } from "src/app/utils/serialize/payment.serialize";
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { PaymentDto } from 'src/app/dto/payment.dto';
+import { PaymentSerialize } from 'src/app/utils/serialize/payment.serialize';
 
 @Injectable()
 export class PaymentService {
-	constructor(@Inject('PAYMENT_SERVICE') private readonly paymentClient: ClientKafka) {
-			
-	}
+  constructor(
+    @Inject('PAYMENT_SERVICE') private readonly paymentClient: ClientKafka,
+  ) {}
 
-	handlePaymentCreate(paymentData: PaymentDto) {
-		this.paymentClient.send("create_payment", new PaymentSerialize(paymentData.item, paymentData.value)).subscribe((payment) => {
-			console.log(`Payment infomation: ${payment.item} and ${payment.value} and ${payment.key}`)
-		})
-	}
+  handlePaymentCreate(paymentData: PaymentDto) {
+    this.paymentClient
+      .send(
+        'create_payment',
+        new PaymentSerialize(paymentData.item, paymentData.value),
+      )
+      .subscribe((payment: { item: string; value: number; key: string }) => {
+        console.log(
+          `Payment infomation: ${payment.item} and ${payment.value} and ${payment.key}`,
+        );
+      });
+  }
 }
